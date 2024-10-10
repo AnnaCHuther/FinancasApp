cb3635148874ee69a2241dc303bda40cb913dcfe
AnnaCHuther <anna.huther@hotmail.com>
2024-10-10 19:44:17 -0300

    first commit

---
+++ FinancasApp.API/Controllers/UsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpPost("Criar")]
+        public IActionResult Criar()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Autenticar")]
+        public IActionResult Autenticar()
+        {
+            return Ok();
+        }
+    }
+}
